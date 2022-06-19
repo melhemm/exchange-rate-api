1f284bd123fce45c52ee887a38fd4bd5f4a18f96
melhemm <jamalmulhemm@gmail.com>
2022-06-20 01:16:47 +0300

    set not found page to router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-        <Routes>
-          <Route path="/about" element={<About/>} />
-          <Route exact path="/" element={<Home/>}/>
-        </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>} />
+            <Route path="*" element={<NotFound/>} /> 
+          </Routes>
+      </Router>
+    </div>
+    
   );
 }
 

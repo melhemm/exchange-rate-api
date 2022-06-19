d85b618f91fdfe8877d1f6ea3cef4e00eef04a3a
melhemm <jamalmulhemm@gmail.com>
2022-06-20 00:01:28 +0300

    render navbar & implement router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/about" element={<About/>} />
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+    </Router>
   );
 }
 

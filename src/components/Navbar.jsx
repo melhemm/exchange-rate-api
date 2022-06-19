255788f25c7bba2303f966a17df42b29d462a169
melhemm <jamalmulhemm@gmail.com>
2022-06-20 00:17:46 +0300

    implement style for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,24 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link to="/" className='navbar-brand'>Exchange Rate</Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link to="/" className='nav-link ml-auto'>Home</Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/about" className='nav-link ml-auto'>About</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 

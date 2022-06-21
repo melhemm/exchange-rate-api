aa993189d7f8eac945a47b0a0b50b2b3687031b9
melhemm <jamalmulhemm@gmail.com>
2022-06-22 01:21:02 +0300

    map is not a function err fixed

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -1,28 +1,34 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const url = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=10'
-
 const CryptoList = () => {
-  const [items, setItems] = useState([])
+  const [coins, setCoins] = useState([]);
+  const API_URL = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=10';
+
+  const fetchData = async () => {
+    await axios.get(API_URL)
+    .then((response) => {
+      return response.data;
+    }).then((data)=> {
+      let coinsData = data.coins
+      console.log(coinsData);
+      setCoins(coinsData)
+    })
+  };
 
   useEffect(() => {
-    const fetchItems = async() => {
-      const result = await axios.get(`${url}`)
-      setItems(result.data)
-      console.log(result);
-    }
-    fetchItems()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <div>
-     <h1>CryptoList</h1> 
-      <section>
-        {items.map((item) => (
-          <h5 key={item.id} >{item.name}</h5>
-        ))}
-      </section>
+      <div>
+       {coins.map(data => (
+        <div key={data.id}>
+          <h2>{data.name}</h2>
+        </div> 
+       ))} 
+      </div>
     </div>
   )
 }

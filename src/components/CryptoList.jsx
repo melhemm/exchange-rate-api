f7df9c597d1a8d811902fd8f2b189d03a44a240d
melhemm <jamalmulhemm@gmail.com>
2022-06-23 01:45:36 +0300

    table for data and style

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -1,5 +1,20 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import './CryptoList.css'
+
+export const formatNumbers = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+export const checkPrice = (p) => {
+  const priceChange = Math.sign(p);
+  if (priceChange === -1) {
+    return "&#8595";
+  }
+  return "&#8593";
+};
+
 
 const CryptoList = () => {
   const [coins, setCoins] = useState([]);
@@ -13,6 +28,8 @@ const CryptoList = () => {
       let coinsData = data.coins
       console.log(coinsData);
       setCoins(coinsData)
+    }).catch((err) => {
+      console.log(err)
     })
   };
 
@@ -22,13 +39,30 @@ const CryptoList = () => {
 
   return (
     <div>
-      <div>
-       {coins.map(data => (
-        <div key={data.id}>
-          <h2>{data.name}</h2>
-        </div> 
-       ))} 
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Rank</th>
+            <th>Price</th>
+            <th>Change 24h</th>
+            <th>MarketCap</th>
+          </tr>
+          {coins.map(data => (
+            <tr key={data.id}>
+              <td>{data.name}
+                <img src={data.icon} alt={data.name} width="30px" height="30px" className='icon-img'/>
+              </td>
+              <td>{data.rank}</td>  
+              <td>{formatNumbers(data.price.toFixed(2))} &#36;</td>
+              <td className={checkPrice(data.priceChange1d)}>
+                {data.priceChange1d}
+              </td>
+              <td>{formatNumbers(data.marketCap.toFixed(2))} &#36;</td>
+            </tr> 
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }

0e7432c6a4aa682240eec9acc66032d79d01261b
melhemm <jamalmulhemm@gmail.com>
2022-06-24 01:04:40 +0300

    dynamic route with params

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Crypto from "./components/Crypto";
+
+// function CryptoId() {
+//   let {cryptoId} = useParams();
+// }
 
 function App() {
   return (
@@ -14,7 +19,8 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>} />
-            <Route path="*" element={<NotFound/>} /> 
+            <Route path="*" element={<NotFound/>} />
+            {<Route path="crypto/:id" element={<Crypto/>} />}
           </Routes>
       </Router>
     </div>

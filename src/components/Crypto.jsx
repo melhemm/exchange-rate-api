e9b3cd2694519e719c495e68d90a58af5a2524ed
melhemm <jamalmulhemm@gmail.com>
2022-06-24 01:06:31 +0300

    fetch crypto by id

---
+++ src/components/Crypto.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+
+const Crypto = () => {
+  const params = useParams()
+
+  const [crypto, setCrypto] = useState('')
+
+  const API_URL = `https://api.coinstats.app/public/v1/coins/${params.id}`;
+
+  const fetchDataById = async () => {
+    await axios.get(API_URL)
+    .then((response) => {
+      return response.data;
+    }).then((data)=> {
+      let coinsData = data.coin
+      console.log(coinsData);
+      setCrypto(coinsData)
+    }).catch((err) => {
+      console.log(err)
+    })
+  };
+
+  useEffect(() => {
+    fetchDataById()
+    console.log("Params", params);
+  }, [params])
+
+  
+
+  return (
+    <div>Crypto</div>
+  )
+}
+
+export default Crypto
+

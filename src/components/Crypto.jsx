aa10f782a32e03a3e8004029d35f1293e71b6282
melhemm <jamalmulhemm@gmail.com>
2022-06-26 01:11:04 +0300

    show crypto details

--- src/components/Crypto.jsx
+++ src/components/Crypto.jsx
@@ -3,6 +3,8 @@ import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { checkPrice, formatNumbers } from './CryptoList';
 
+// console.log(formatNumbers(4567891313)); 
+
 const Crypto = () => {
   const params = useParams()
 
@@ -18,6 +20,8 @@ const Crypto = () => {
       let coinsData = data.coin
       console.log(coinsData);
       setCrypto(coinsData)
+      let priceText = coinsData.price
+      console.log((priceText.toFixed(2)))
     }).catch((err) => {
       console.log(err)
     })
@@ -31,11 +35,18 @@ const Crypto = () => {
   
 
   return (
-    <div>
+    <div className='container'>
       <h1>Crypto details</h1>
+      <img src={crypto.icon} alt={crypto.name} width="30px" height="30px" className='icon-img'/>
       <h3>{crypto.name}</h3>
       <h4>{crypto.rank}</h4>
-      <p>&#36;{crypto.price}</p>
+      <p>&#36; {crypto.price}</p>
+      <p>{crypto.priceChange1d}</p>
+      <p>{crypto.priceChange1h}</p>
+      <p>{crypto.priceChange1w}</p>
+      <p>&#36;{crypto.marketCap}</p>
+      <p>&#36;{crypto.totalSupply}</p>
+      <p>{crypto.websiteUrl}</p>
     </div>
   )
 }

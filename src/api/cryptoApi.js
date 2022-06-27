60ec934debecfbb0b4bbbcb675a373136fbee437
melhemm <jamalmulhemm@gmail.com>
2022-06-28 01:18:33 +0300

    remove currency parameter

--- src/api/cryptoApi.js
+++ src/api/cryptoApi.js
@@ -4,5 +4,5 @@ export const CoinList = () =>
 export const SingleCoin = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
-export const HistoricalChart = (id, days = 365, currency) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+export const HistoricalChart = (id, days = 365) =>
+  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=rub&days=${days}`;
